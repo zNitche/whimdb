4beb83388b288c4f8633f1c73c3b733b59c7d788
zNitche <53448802+zNitche@users.noreply.github.com>
2025-11-02 17:42:56 +0100

    feat: cli entrypoint for running server

---
+++ whimdb/cli/whimdb_server.py
@@ -0,0 +1,27 @@
+import argparse
+from whimdb import Server
+
+
+def main(args: argparse.Namespace):
+    server = Server(port=args.port, addr=args.addr, debug=args.debug)
+    server.run()
+
+
+def get_args():
+    argument_parser = argparse.ArgumentParser()
+
+    argument_parser.add_argument(
+        "--port", type=int, default=8080, help="server port", required=False)
+    argument_parser.add_argument(
+        "--addr", type=str, default="0.0.0.0", help="server address", required=False)
+
+    argument_parser.add_argument("--debug", action=argparse.BooleanOptionalAction,
+                                 type=bool, default=False, help="debug mode", required=False)
+
+    return argument_parser.parse_args()
+
+
+def run():
+    args = get_args()
+
+    main(args)
