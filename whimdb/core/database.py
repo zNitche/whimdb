201e4e268bb416139e7b0278a75cb122e5801ad3
zNitche <53448802+zNitche@users.noreply.github.com>
2025-11-08 20:58:17 +0100

    feat: base database implementation

--- whimdb/core/database.py
+++ whimdb/core/database.py
@@ -1,3 +1,12 @@
+from typing import Any
+
+
 class Database:
     def __init__(self):
-        pass
+        self.__content = {}
+
+    def set(self, key: str, value: Any):
+        self.__content[key] = value
+
+    def query(self, key: str, regex_string: str | None = None):
+        return self.__content.get(key)
