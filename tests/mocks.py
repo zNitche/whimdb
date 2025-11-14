3382bfba8a26dab8c131cf064a6c0111ecacdbc9
zNitche <53448802+zNitche@users.noreply.github.com>
2025-11-14 22:59:55 +0100

    test: updated mocks for packets related tests

--- tests/mocks.py
+++ tests/mocks.py
@@ -3,7 +3,7 @@ from whimdb.dataclasses.communication import Request
 from whimdb.core import Packet
 
 
-PACKET_BUFF = b'\x00\x03{"response": null, "request": {"database_id": 0, "key": "test_key", "value": "test_value", "search_regex": null, "ttl": null, "items_per_page": 20, "page_id": 0}}'
+PACKET_BUFF = b'\x00\x04{"response": null, "request": {"database_id": 0, "key": "test_key", "value": "test_value", "search_regex": null, "ttl": null, "items_per_page": 20, "page_id": 0}}'
 PACKET_RAW_BUFF = b'\x00\x00\x00\xa4' + PACKET_BUFF
 PACKET = Packet(type=PacketTypeEnum.SET,
-                content=PacketContent(request=Request(database_id=0, key="test_key", value="test_value", items_per_page=20)))
+                content=PacketContent(request=Request(database_id=0, key="test_key", value="test_value", items_per_page=20, page_id=0)))
