efe62e69df63e68a00f3e55abb685400031849c6
zNitche <53448802+zNitche@users.noreply.github.com>
2025-11-21 23:23:21 +0100

    fix: ExpiredItemsCleanupTask accessing database content

--- whimdb/tasks/expired_items_cleanup_task.py
+++ whimdb/tasks/expired_items_cleanup_task.py
@@ -16,7 +16,9 @@ class ExpiredItemsCleanupTask(TaskBase):
 
             for db_id in self.__dbs:
                 keys_to_remove: list[str] = []
-                db_content = self.__dbs[db_id].content
+
+                db = self.__dbs[db_id]
+                db_content = db.content
 
                 for key in db_content:
                     db_item = db_content[key]
@@ -28,11 +30,11 @@ class ExpiredItemsCleanupTask(TaskBase):
                         keys_to_remove.append(key)
 
                 for key in keys_to_remove:
-                    del db_content[key]
+                    db.remove(key=key)
 
                 removed_count = len(keys_to_remove)
 
                 if removed_count > 0:
-                    self._logger.info(f"done, removed {removed_count} items from db id:{db_id}")
+                    self._logger.info(f"done, removed {removed_count} items from db:{db_id}")
 
             await asyncio.sleep(10)
