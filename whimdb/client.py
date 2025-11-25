593a1c7935fcdfa709a847db5c27f6893867577d
zNitche <53448802+zNitche@users.noreply.github.com>
2025-11-25 21:01:19 +0100

    feat: client - setting ttl_left

--- whimdb/client.py
+++ whimdb/client.py
@@ -178,6 +178,10 @@ class Client:
 
         item.is_expired = is_expired
 
+        if item.ttl is not None:
+            ttl_left = int(item.created_at + item.ttl - current_time)
+            item.ttl_left = ttl_left if ttl_left >= 0 else 0
+
         return item
 
     def __send_packet(self, packet: Packet):
